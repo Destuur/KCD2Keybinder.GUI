a90100038a91ef62835f7acc0e20f3f03bee9caf
Michael Weichart <m.weichart@gmail.com>
2026-01-25 23:35:17 +0100

    fix static game directory

--- KCD2Keybinder.Core/Utils/Resources.cs
+++ KCD2Keybinder.Core/Utils/Resources.cs
@@ -8,7 +8,7 @@ namespace KDC2Keybinder.Core.Utils
 {
 	public static class Resources
 	{
-		public static string GameDirectory => "G:\\SteamLibrary\\steamapps\\common\\KingdomComeDeliverance2";
+		public static string GameDirectory => "";
 		public static string DataDirectory => Path.Combine(GameDirectory, "Data");
 		public static string ModsDirectory => Path.Combine(GameDirectory, "Mods");
 		public static string IPLGameData => "IPL_GameData.pak";
